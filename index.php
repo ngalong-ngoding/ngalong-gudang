64dd806a6632fc28c8d5641a7b9f9736b8da46e7
unknown <rohim.maruf26@gmail.com>
2024-10-08 20:14:44 +0700

    Punya Rohim

--- index.php
+++ index.php
@@ -3,11 +3,14 @@
 // dihalaman ini itu digunain semacam untuk pintu masuk utamanya (route)
 
 require_once "./controllers/FatahController.php";
+require_once "./controllers/RohimContoller.php";
 
 $fatahController = new FatahController();
+$rohimController = new RohimContoller();
 
 $routes = [
     "fatah" => [$fatahController, "index"],
+    "rohim" => [$rohimController, "index"],
     // nanti kalau mau ditambahin berarti
     // "routenya" => [$namaController, "index"];
 ];
@@ -15,6 +18,7 @@ $routes = [
 // Ini buat ngambil route yang ada di url kita, misalnya localhost/ngalong-gudang/index.php?route=fatah
 // berarti yang diambil sama GET["route"] = "fatah" nya
 $action = $_GET["route"] ?? header("Location: fatah");;
+$action = $_GET["route"] ?? header("Location: rohim");;
 
 // Ini buat ngecek si variabel $action itu beneran ada gak di $routes
 if (array_key_exists($action, $routes)) {
---
+++ models/rohim.php
@@ -0,0 +1,12 @@
+<?php
+class Rohim
+{
+    public function getAllData()
+    {
+        return[
+                ["id" => 1, "hobby" => "Ngising", "Kesukaan" => "Makan"],
+                ["id" => 2, "hobby" => "Nyanyi", "Kesukaan" => ""] 
+        ];
+    }
+}
+?>

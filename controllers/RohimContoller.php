896641126d2981b3c27a13feb68029fec529cad9
rohimmaruf <rohim.maruf26@gmail.com>
2024-10-14 21:12:22 +0700

    Menambahkan fitur rambah, edit dan hapus

--- controllers/RohimContoller.php
+++ controllers/RohimContoller.php
@@ -4,12 +4,68 @@ require_once "./models/rohim.php";
 
 
 class RohimContoller
-{
+{   
+    // 1. Membuat public fungcition
     public function index()
     {
+        // 2. Membuat Variable Baru dengan = nama class yang di models
         $rohimModel = new Rohim();
-        $data = $rohimModel->getAllData();
+        // 3. Membuat variable data dimana variable diatas dan di panggil fungsinya
+        $data = $rohimModel->getDaftarBarang();
         include "./views/rohim.php";
+    }
+    public function store(){
+        // 4. Untuk mengecek atau logic "ada yang post pake name submit gak"
+        
+        if (isset($_POST["submit"])){
+            // 4.1. Ini logic buat namabah data
+            header("Location: rohim");
+            $rohim = new Rohim();
+            // 4.2 ini nyimpen hasil ketika kita input
+            $barang = $_POST["barang"];
+            // 4.3 ini masukin input ke model (tambah barang)
+            $rohim->tambahBarang($barang);
+            // 4.4 Buat balik algi ke halaman awal
+            
+        }
+    }
+    // 5. Membuat fungsi untuk delete
+    public function delete(){
+        // 5.1 Membuat variable baru dengan nama class yang di models
+        $rohim = new Rohim();
+        // 5.2 Mengambil id dari Query
+        $id = $_GET["id"] ?? null;
+        if($id){
+            // 5.3 Membuat fungsi delete
+            $deleted = $rohim->hapusBarang($id);
+            if($deleted){
+                header("Location: rohim");
+                exit;
+            }else {
+                echo "Barang tidak ditemukan";
+            }
+        }else {
+            echo"ID tidak valid";
+        }
+    }
+    // 6. Membuat fungsi edit
+    public function edit(){
+        // 6.1 Membuat variable baru dengan nama class yang di models
+        $rohim = new Rohim();
+        // 6.2 Mengambil id dari Query
+        $id = $_GET["id"] ?? null;
+        $newBarang = $_POST["barang"];
+        if($id && $newBarang){
+            // 6.3 Membuat fungsi edit
+            $updated = $rohim->editBarang($id, $newBarang);
+            if($updated){
+                header("Location: rohim");
+            }else {
+                echo "Barang tidak ditemukan";
+            }
+        }else{
+            echo "ID tidak valid";
+        }
     }
 }
 ?>
--- models/rohim.php
+++ models/rohim.php
@@ -1,12 +1,67 @@
 <?php
 class Rohim
 {
-    public function getAllData()
+    // 1. Membuat Variable terlebih dahulu
+    private $daftarBarang = [];
+
+    // 2. Membuat fungsi yang berisi kondisi jika ada data dari daftar barang maka data daftar barang yang akan terisi
+    public function __construct()
     {
-        return[
-                ["id" => 1, "hobby" => "Ngising", "Kesukaan" => "Makan"],
-                ["id" => 2, "hobby" => "Nyanyi", "Kesukaan" => ""] 
+        // 2.1. isset = "ada data gak nih"  dari variable daftar barang rohim dmn daftar barang rohim tuh sama dengan daftarBarang 
+        if (isset($_SESSION["daftarBarangRohim"])) 
+        {
+            
+            $this->daftarBarang = $_SESSION["daftarBarangRohim"];
+        } else {
+            $this->daftarBarang = [
+                ["id" => 1, "barang" => "Pepsodent"],
+                ["id" => 2, "barang" => "CloseUp"],
+                ["id" => 3, "barang" => "Kodomo"],
+            ];
+        }
+    }
+    // 3. Membuat Fungsi Agar bisa dipanggil oleh views agar bisa dipanggil oleh seluruh kontruksi
+    public function getDaftarBarang(){
+        return $this->daftarBarang;
+    }
+    // 4. Membuat fungsi untuk menambah daftar barang
+    public function tambahBarang($barang){
+        // 4.1. Mewmbuat variable baru dengan fungsi menambah panjang data dari daftar barang
+        $newId = count($this->daftarBarang) + 1;
+        // 4.2. ini Array lohi isinya yang dibawah ini ya
+        $this->daftarBarang[] = [
+            "id" => $newId,
+            "barang" => $barang
         ];
+        // 4.3. Sekalina ini seison store tadi
+        $_SESSION["daftarBarangRohim"] = $this->daftarBarang;
+    }
+    // 5 Membuat fungsi untuk menghapus daftar barang
+    public function hapusBarang($id){
+        // 5.1. Membuat variable baru dengan fungsi menghapus data dari data
+        // 5.2. Membuat looping untuk mencari id sama sama yang dimiliki
+        foreach($this->daftarBarang as $key => $barang){
+            if($barang["id"] == $id){
+                // 5.3. Fungsi hapus game dari dafter
+                unset($this->daftarBarang[$key]);
+                // 5.4. Update variable yang di atas
+                $this->daftarBarang = array_values($this->daftarBarang);
+                $_SESSION["daftarBarangRohim"] = $this->daftarBarang;
+                return true;
+            }
+        }
+    }
+    // 6. Membuat fungsi untuk edit barang
+    public function editBarang($id, $newBarang){
+        foreach ($this->daftarBarang as $key => $barang){
+            if ($barang["id"] == $id) {
+                // 6.1 Update nama gamenya
+                $this->daftarBarang[$key]["barang"] = $newBarang;
+                // 6.2 Update variable di atas
+                $_SESSION["daftarBarangRohim"] = $this->daftarBarang;
+            return true;
+            }
+        }
     }
 }
 ?>

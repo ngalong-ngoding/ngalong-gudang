65cad019d9a9857dc00c1a7b0fc5726e83f1eed7
Muhamad_Fatah <muhamad.fatah494@gmail.com>
2024-10-20 14:35:12 +0700

    update barang.sql to insert item

--- DB/barang.sql
+++ DB/barang.sql
@@ -14,6 +14,19 @@ CREATE TABLE `barang` (
     `foto_barang` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data untuk tabel `barang`
+--
+
+INSERT INTO `barang` (`id`, `nama_barang`, `jumlah`, `kategori`, `deskripsi`, `foto_barang`) VALUES
+(27, 'Kodomo', '2', 'kecantikan', 'Produk ini adalah produk yang bagus', 'kodomo.jpg'),
+(28, 'Makanan Ikan', '10', 'Makanan', 'Makanan ikan enak tau', 'makanan-ikan.png'),
+(29, 'Keyboard', '1', 'Komputer', 'Keyboard yummy yummy', 'keyboard.jpeg'),
+(30, 'Monitor', '2', 'Komputer', 'Monitor Viewsonic', 'monitor.webp'),
+(31, 'Ikan cupang', '10', 'Hewan', 'Ikan laga-laga', 'cupang.jpg'),
+(32, 'Laptop', '1', 'Komputer', 'Laptop eroji', 'laptop.webp'),
+(33, 'Speaker', '1', 'Komputer', 'Speaker jedag jedug', 'speaker.jpg');
+
 -- Buat kasih primary key ke id
 ALTER TABLE `barang`
     ADD PRIMARY KEY (`id`);
